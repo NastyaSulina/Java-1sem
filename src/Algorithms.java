06fe3ad6f8c407ba7232d0ff9924cbb2ac7d814e
Настя <sulina01@mail.ru>
2021-11-18 18:18:56 +0300

    Разложение числа на простые множители (задача 3)

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Algorithms {
     public static void main(String[] args){
         System.out.println(Arrays.toString(sieveOfEratosthenes(30)));
 
         System.out.println(Arrays.toString(primes(30)));
+
+        System.out.println(Decomposition(56));
     }
     private static boolean[] sieveOfEratosthenes(int n){
         boolean[] prime = new boolean[n + 1];
@@ -28,6 +32,29 @@ public class Algorithms {
                 result[j] = i;
                 j++;
             }
+        return result;
+    }
+    private static List<List<Integer>> Decomposition(int number){
+        int[] primesList = primes(number);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int d : primesList) {
+            if (d*d > number ) {
+                if (number != 1) {
+                    List<Integer> pair = List.of(number, 1);
+                    result.add(pair);
+                }
+                break;
+            }
+            int count = 0;
+            while (number % d == 0) {
+                number = number / d;
+                count++;
+            }
+            if (count != 0) {
+                List<Integer> pair = List.of(d, count);
+                result.add(pair);
+            }
+        }
         return result;
     }
 }
